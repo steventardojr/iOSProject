5c965f4b4acf9d0589ae80ece566b4cdcfae06fe
Steven Tardo <stardo@uno.edu>
2015-02-08 12:25:34 -0600

    Initial Commit

---
+++ Super Fun Sudoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Fun Sudoku
+//
+//  Created by Steven Tardo Jr. on 2/8/15.
+//  Copyright (c) 2015 CSCI 4661. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
