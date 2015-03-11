7cbc66adf6f9d8dae8a3eecb830f8c3634fb9177
Steven Tardo <stardo@uno.edu>
2015-03-11 17:21:55 -0500

    added views

---
+++ Super Fun Sudoku/BoardView.swift
@@ -0,0 +1,24 @@
+//
+//  BoardView.swift
+//  Super Fun Sudoku
+//
+//  Created by Steven Tardo Jr. on 3/11/15.
+//  Copyright (c) 2015 CSCI 4661. All rights reserved.
+//
+
+import UIKit
+
+
+class BoardView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
